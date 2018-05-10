186a4363173e6cecbdccccbd3217d7512cf36393
Gerard Parareda Gallifa <gerard.parareda.2015@salleurl.edu>
2018-05-10 19:43:15 +0200

    Començada validació. Falta comprovar que funciona

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -23,9 +23,28 @@ function validateForm(){
     var birth = birthMonth.toString();
     if(birth == "January" || birth == "March" || birth == "May" || birth == "July" || birth == "August" || birth == "October" || birth == "December"){
         if(birthDay.toString() > 31 || birthDay.toString() < 1){
-            document.getElementById("error-email").innerHTML = "Dia ivàlid";
+            document.getElementById("error-email").innerHTML = "Invalid day";
+        }
+    }else if(birth == "February"){
+        if(birthDay.toString() > 28 || birthDay.toString() < 1){
+            document.getElementById("error-email").innerHTML = "Invalid day";
+        }
+    } else {
+        if(birthDay.toString() > 30 || birthDay.toString() < 1){
+            document.getElementById("error-email").innerHTML = "Invalid day";
         }
     }
+
+    var password_s = password.toString();
+    if (password_s.length < 6 || password_s > 12){
+        document.getElementById("error-password").innerHTML = "Password length must be between 6 and 12 characters";
+    }
+    if (password_s.toLowerCase() !== password_s && password_s.toUpperCase() !== password_s){
+
+    } else {
+        document.getElementById("error-password").innerHTML = "Password must contain at least one number and one upper case letter";
+    }
+
 
     return errors;
 
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -28,15 +28,76 @@ class RegisterController{
         try{
             $data = $request->getParsedBody();
             var_dump($data);
+
+            $errors = [];
+
             //Validate
-            //if (isset($data['email'])) {}
-            //$service = $this->container->get('post_user_use_case');
-            //$service($data);
+            if (isset($data['inputEmail']) && filter_var($data['inputEmail'], FILTER_VALIDATE_EMAIL)) {
+
+            } else {
+                $errors[] = 'errorEmail';
+            }
+
+            if (isset($data['inputUsername']) && strlen($data['inputUsername']) <= 20 && preg_match("[^A-Za-z0-9]+", $data['inputUsername'])) {
+
+            } else {
+                $errors[] = 'errorUsername';
+            }
+
+            if (isset($data['inputBirthDay']) && isset($data['inputMonthBirth']) && isset($data['inputBirthYear']) && $this->validateDay($data['inputBirthDay'], $data['inputMonthBirth'])) {
+
+            } else {
+                $errors[] = 'errorBirth';
+            }
+
+            if (isset($data['inputPassword'])) {
+
+                if (strlen($data['inputPassword']) < 6 || strlen($data['inputPassword']) > 12){
+                    $errors[] = 'errorPasswordLength';
+                }
+                if (strtolower($data['inputPassword']) != $data['inputPassword'] || strtoupper($data['inputPassword']) != $data['inputPassword']){
+
+                } else {
+                    $errors[] = 'errorPasswordCase';
+                }
+
+            } else {
+                $errors[] = 'errorPassword';
+            }
+
+            if (isset($data['inputPasswordConf'])) {
+                 if ($data['inputPasswordConf'] != $data['inputPassword']){
+                     $errors[] = 'errorPasswordMismatch';
+                 }
+            } else {
+                $errors[] = 'errorPasswordMismatch';
+            }
+
+
+            return $this->container->get('view')->render($response, 'register.twig', ['error_array' => $errors]);
 
         }catch(\Exception $e){
             $response = $response->withStatus(500)->withHeader('Content-type', 'text/html')->write('Something went wrong');
         }
         return $response;
+    }
+
+    private function validateDay($day, $month){
+
+        if($month == "January" || $month == "March" || $month == "May" || $month == "July" || $month == "August" || $month == "October" || $month == "December"){
+            if($day > 31 || $day < 1){
+               return false;
+            }
+        } else if ($month =="February"){
+            if ($day > 28 || $day < 1){
+                return false;
+            }
+        } else {
+            if ($day > 30 || $day < 1){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
