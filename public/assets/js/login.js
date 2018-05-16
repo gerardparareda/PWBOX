2b2ee951996cc6ef58968bfd2d3fed24b928aa3c
ArnauMias <arnau.mias.2015@salleurl.edu>
2018-05-16 10:38:44 +0200

    Millorada comprovacio front end login per fer l'opcional.

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -4,26 +4,45 @@ function validateLogin() {
     var password = document.forms["form-signin"]["inputPassword"].value;
     var errors = true;
 
-    alert("He arribat aqui");
+    var arroba = /@/;
 
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //if (arroba.test(email)) {
+    if (arroba.test(email)) {
 
-    if (!re.test(String(email).toLowerCase())) {
-        document.getElementById("error-email").innerHTML = "Email invàlid.";
-        errors = false;
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (!re.test(String(email).toLowerCase())) {
+            //document.getElementById("error-email").innerHTML = "Email invàlid.";
+            errors = false;
+        }
+
+        console.log('Es un email.');
+
+    } else {
+
+        if (email.toString().length > 20 || email.toString().match("[^A-Za-z0-9]+")) {
+            document.getElementById("error-nom").innerHTML = "Nom d'usuari invàlid. Insereixi només majúscules, minúscules i números.";
+            errors = false;
+        }
+
+        console.log('Es un username.');
+
     }
+
 
     var password_s = password.toString();
     if (password_s.length < 6 || password_s > 12) {
-        document.getElementById("error-password").innerHTML = "Password incorrecte. ";
+        //document.getElementById("error-password").innerHTML = "Password incorrecte. ";
         errors = false;
     }
     if (password_s.toLowerCase() !== password_s && password_s.toUpperCase() !== password_s) {
 
     } else {
-        document.getElementById("error-password").innerHTML = "Password incorrecte ";
+        //document.getElementById("error-password").innerHTML = "Password incorrecte ";
         errors = false;
     }
+
+    console.log('aaaa');
 
     if (errors == false) {
         /*divError = document.createElement('p');
@@ -38,9 +57,14 @@ function validateLogin() {
         divError.classList.add("text-danger");*/
         divError.innerText = "Usuari o password incorrectes";
 
-        element = document.getElementById("inputPassword");
-        element.parentNode.insertBefore(divError, element.nextSibling);
-        console.log('hola');
+        if (document.getElementById('errors') == null) {
+
+            element = document.getElementById("inputPassword");
+            divError.id = 'errors';
+            element.parentNode.insertBefore(divError, element.nextSibling);
+            console.log('hola');
+
+        }
 
     }
 
