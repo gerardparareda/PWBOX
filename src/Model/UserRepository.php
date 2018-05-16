e09cbe7e67cddd76d7d3003103afffef8df814ff
ArnauMias <arnau.mias.2015@salleurl.edu>
2018-05-16 19:48:23 +0200

    Afegits metodes a la bbdd per crear i mostrar carpetes i get email i username a partir de l'id de l'usuari.

--- src/Model/UserRepository.php
+++ src/Model/UserRepository.php
@@ -5,4 +5,7 @@ namespace PwBox\Model;
 interface UserRepository{
     public function save(User $user);
     public function lookFor(User $user);
+    public function getUserId(User $user);
+    public function getEmailById(int $idUsuari);
+    public function getUsernameById(int $idUsuari);
 }
